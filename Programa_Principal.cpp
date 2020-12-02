e5924413780f19bc3fb0d16d686cf52fcfee85cf
Brandan Marcos <Marcos.Brandan@alu.utn.edu.ar>
2020-12-02 10:56:18 -0300

    Agregar mas opcion de menu al archivo Programa_Principal.cpp y realizar modificaciones

--- Programa_Principal.cpp
+++ Programa_Principal.cpp
@@ -4,6 +4,8 @@
 
 int MenuPrincipal();
 int MenuConsultorio();
+int MenuAsistente();
+int MenuAdministracion();
 
 main()
 {
@@ -19,9 +21,62 @@ main()
 			case 1: do{
 				 	   opc = MenuConsultorio();
 				 	   switch(opc){
+				 	   	
+				 	   	case 1: 
+				 	   	break;
+				 	   	
+				 	   	case 2:
+			 	   		break;
+			 	   		
+			 	   		case 3:
+			 	   		break;
    			   		   //agregar llamado a las correspondientes funciones
 						}
 				 	}while(opc =! 4);
+			break;
+			
+			case 2: do{
+				 	   opc = MenuAsistente();
+					   
+					   switch(opc){
+					   	
+					   	case 1:
+				   		break;
+				   		
+				   		case 2:
+			   			break;
+			   			
+			   			case 3:
+		   				break;
+		   				
+		   				case 4:
+	   					break;
+					   	
+					   }
+				
+					}while(opc != 5);
+			break;
+			
+			case 3:	do{
+				 	    opc = MenuAdministracion();
+				 		
+				 		switch(opc){
+			 			
+			 			case 1:
+		 				break;
+		 				
+		 				case 2:
+	 					break;
+			 			
+			 			case 3:
+		 				break;
+		 				
+		 				case 4:
+	 					break;
+	 					}	
+				 	
+				 	}while(opc != 5);
+			
 			break;
 		}
 		
@@ -33,9 +88,9 @@ main()
 int MenuPrincipal(){
 	system("cls");
 	int opc;
-	printf("1 --> MODULO CONSULTORIO");
-	printf("1 --> MODULO DEL ASISTENTE");
-	printf("1 --> MODULO DE ADMINISTRACION");
+	printf("1 --> MODULO CONSULTORIO\n");
+	printf("2 --> MODULO DEL ASISTENTE\n");
+	printf("3 --> MODULO DE ADMINISTRACION\n");
 	scanf("%d", &opc);
 	return opc;
 }
@@ -44,17 +99,40 @@ int MenuConsultorio(){
 	
 	system("cls");
 	int opc;
-	printf("1.- Iniciar sesion");
-	printf("2.- Visualizar Lista de Espera de Turnos");
-	printf("3.- Registrar Evolución de la Mascota\n");
-	printf("4.- Cerrar la aplicación");
+	printf("1.- Iniciar sesion\n");
+	printf("2.- Visualizar Lista de Espera de Turnos\n");
+	printf("3.- Registrar Evolución de la Mascota\n\n");
+	printf("4.- Cerrar la aplicación\n");
 	scanf("%d", &opc);
 	return opc;
 	
 }
 
+int MenuAsistente(){
+	
+	system("cls");
+	int opc;
+	printf("1.- Iniciar sesion\n");
+	printf("2.- Registrar mascota\n");
+	printf("3.- Registrar turno\n");
+	printf("4.- Listado de Atenciones por Veterinario y Fecha \n\n");
+	printf("5.- Cerrar la aplicación\n");
+	scanf("%d", &opc);
+	return opc;
+}
 
-
-
+int MenuAdministracion(){
+	
+	system("cls");
+	int opc;
+	printf("1.- Registrar Vetrinario\n");
+	printf("2.- Registrar Usuario Asitente\n");
+	printf("3.- Atenciones por veterinarios\n");
+	printf("4.- Ranking de Veterinarios por Atenciones\n\n");
+	printf("5.- Cerrar la aplicación\n");
+	scanf("%d", &opc);
+	return opc;
+	
+}
 
 
