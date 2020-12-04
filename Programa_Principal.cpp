6e872b6800a6e9a16b6c42bb26837e72e4c8b88b
Brandan Marcos <Marcos.Brandan@alu.utn.edu.ar>
2020-12-04 15:11:01 -0300

    Modificaciones en el archivo Programa_Principal.cpp

--- Programa_Principal.cpp
+++ Programa_Principal.cpp
@@ -21,9 +21,10 @@ bool verificar = false;
 main()
 {
 	FILE *usuarios = fopen("Usuarios.dat","a+b");
+	
 	char lectura;
 	cadena nombreUsuario, contr;
-	
+	rewind(usuarios);
 	//verifica si ya hay un usuario registrado
 	fread(&lectura, sizeof(lectura), 1, usuarios);
 	while(!feof(usuarios)){
@@ -35,39 +36,36 @@ main()
 	//caso que no exista ningun usuario, permite cargar uno
 	if(verificar == false){
 		registrar(usuarios);
+		verificar = true;
 	}
-	system("cls");
-	
-	do{
-		printf("\nINICIO DE SESION\n");
-		printf("\nNmbre de usuario: ");
-		_flushall();
-		gets(nombreUsuario);
+	else{
 		
-		Usuarios lect;
+		do{
+		   rewind(usuarios);
+		   printf("\nINICIO DE SESION\n");
+		   printf("\nNmbre de usuario: ");
+		   _flushall();
+		   gets(nombreUsuario);
+		
+		   Usuarios lect;
 			   				
-		fread(&lect, sizeof(lect), 1, usuarios);
+			fread(&lect, sizeof(lect), 1, usuarios);
 			   				
-		while(!feof(usuarios)){
+			while(!feof(usuarios)){
 			   					
-			if(strcmp(nombreUsuario, lect.nomUsuario) == 0){
-				verificar = true;
+				if(lect.nomUsuario == nombreUsuario){
+					verificar = true;
 				break;
+				}
+			
+				fread(&lect, sizeof(lect), 1, usuarios);
+			}
+			if(verificar == false){
+				printf("Nombre de usuario incorrecto.");
 			}
 			
-			fread(&lect, sizeof(lect), 1, usuarios);
-		}
-		if(verificar == false){
-			printf("Nombre de usuario incorrecto.");
-		}
-	}while(verificar == false);
-	
-		
-	//comprobar nombre de usuario
-	//printf("\nContraseña: ");
-	//gets(contr);
-	//comprobar contraseña
-	
+		}while(verificar == false);
+	}
 	
 	
 	int opcion;
@@ -80,64 +78,13 @@ main()
 		opcion = MenuPrincipal();
 		
 		switch(opcion){
-			case 1: do{
-				 	   opc = MenuConsultorio();
-				 	   switch(opc){
-				 	   	
-				 	   	case 1: 
-				 	   	break;
-				 	   	
-				 	   	case 2:
-			 	   		break;
-			 	   		
-			 	   		case 3:
-			 	   		break;
-   			   			}
-				 	}while(opc =! 4);
+			case 1: 
 			break;
 			
-			case 2: do{
-				 	   opc = MenuAsistente();
-					   
-					   switch(opc){
-					   	
-					   	case 1:
-				   		break;
-				   		
-				   		case 2:
-			   			break;
-			   			
-			   			case 3:
-		   				break;
-		   				
-		   				case 4:
-	   					break;
-					   	
-					   }
-				
-					}while(opc != 5);
+			case 2: 
 			break;
 			
-			case 3:	do{
-				 	    opc = MenuAdministracion();
-				 		
-				 		switch(opc){
-			 			
-			 			case 1:
-		 				break;
-		 				
-		 				case 2:
-	 					break;
-			 			
-			 			case 3:
-		 				break;
-		 				
-		 				case 4:
-	 					break;
-	 					}	
-				 	
-				 	}while(opc != 5);
-			
+			case 3:	
 			break;
 		}
 		
@@ -163,7 +110,7 @@ int MenuPrincipal(){
 	return opc;
 }
 
-int MenuConsultorio(){
+/*int MenuConsultorio(){
 	
 	system("cls");
 	int opc;
@@ -174,34 +121,7 @@ int MenuConsultorio(){
 	scanf("%d", &opc);
 	return opc;
 	
-}
-
-int MenuAsistente(){
-	
-	system("cls");
-	int opc;
-	printf("1.- Iniciar sesion\n");
-	printf("2.- Registrar mascota\n");
-	printf("3.- Registrar turno\n");
-	printf("4.- Listado de Atenciones por Veterinario y Fecha \n\n");
-	printf("5.- Cerrar la aplicación\n");
-	scanf("%d", &opc);
-	return opc;
-}
-
-int MenuAdministracion(){
-	
-	system("cls");
-	int opc;
-	printf("1.- Registrar Vetrinario\n");
-	printf("2.- Registrar Usuario Asitente\n");
-	printf("3.- Atenciones por veterinarios\n");
-	printf("4.- Ranking de Veterinarios por Atenciones\n\n");
-	printf("5.- Cerrar la aplicación\n");
-	scanf("%d", &opc);
-	return opc;
-	
-}
+}*/
 
 void registrar(FILE *usuario){
 	
@@ -250,7 +170,7 @@ void registrar(FILE *usuario){
 						if(contadorNum < 4){
 			   			    bandera = true;
 			   				
-			   				rewind(usuario);
+			   				/*rewind(usuario);
 			   				fread(&lectura, sizeof(lectura), 1, usuario);
 			   				
 			   				while(!feof(usuario)){
@@ -259,32 +179,19 @@ void registrar(FILE *usuario){
 			   						bandera = false;
 								}
 									
-			   					fread(&lectura, sizeof(lectura), 1, usuario);
+			   					fread(&lectura, sizeof(lectura), 1, usuario);*/
 							}
 			   				//valida que no exista un usuario igual
 						}
 					}
 			}
-		}
 		if(bandera == false){
 			printf("\nEl nombre de usuario ingresado no cumple con alguna de las condiciones dadas.");
 			printf("\nVuelva a intentar.");
 		}
 	}while(bandera == false);
 	
-	//validar las restricciones
-	
-	/*printf("\nContrasenia: ");
-	printf("\nAVISOS IMPORTANTES!\n");
-	printf("\nDebera contener al menos una letra mayuscula, una letra minuscula y un numero");
-	printf("\nNo podra contener ningun caracter de puntuacion, ni acentos, ni espacios. Solo caracteres alfanumericos. ");
-	printf("\nDebera tener entre 6 y 32 caracteres.");
-	printf("\nNo debe tener más de 3 caracteres numericos consecutivos.");
-	printf("\nNo debe tener 2 caracteres consecutivos que refieran a letras alfabeticamente consecutivas (ascendentemente).");
-	gets(admin.contrasenia);
-	//validar restricciones
-	fwrite(&admin, sizeof(admin), 1, usuario);*/
-	fwrite(&lectura, sizeof(lectura), 1, usuario);
+	fwrite(&admin, sizeof(admin), 1, usuario);
 }
 
 
